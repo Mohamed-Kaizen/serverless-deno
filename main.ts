010a204628e7e777ac994a0859044b7b352b333b
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-01-20 11:10:33 +0300

    remove dead code

--- main.ts
+++ main.ts
@@ -1,25 +1,9 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import {
-	parse,
-	stringify,
-} from "https://deno.land/std@0.173.0/encoding/toml.ts";
 
 import { PORT } from "./core/settings.ts";
 import { core_router } from "./core/functions.ts";
 
-// const obj = {
-// 	bin: [
-// 		{ name: "deno", path: "cli/main.rs" },
-// 		{ name: "deno_core", path: "src/foo.rs" },
-// 	],
-// 	nib: [{ name: "node", path: "not_found" }],
-// };
 
-// const tomlString = stringify(obj);
-
-// console.log(tomlString);
-
-// Deno.writeTextFile("Cargo.toml", tomlString);
 const app = new Application();
 
 const router = new Router();
@@ -39,10 +23,6 @@ for await (const file of Deno.readDir("functions")) {
 		router.all(`/${name}`, fn);
 	}
 }
-
-router.get("/", (ctx) => {
-	ctx.response.body = "Hello World!";
-});
 
 router.use(core_router.routes());
 
